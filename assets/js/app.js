53d8b0d9b7f40ad544521083530717358c740753
mmm <margaret.m.matson@gmail.com>
2022-11-06 11:01:39 -0600

    added ids to elements in search section to html and added functions to get elements in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-$(document).foundation()
+$(document).foundation();
+
+cityInput = document.getElementById("city-input");
+searchBtn = document.getElementById("search-button");
+historyContainer = document.getElementById("history");
+
