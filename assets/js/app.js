fb87385ea57b3e2f92e8ceee3d0c6f3297be5248
mmm <margaret.m.matson@gmail.com>
2022-11-06 21:55:11 -0600

    added event listener to search button to append search results to history div and find coordinates for city entered

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,1 @@
 $(document).foundation();
-
-cityInput = document.getElementById("city-input");
-searchBtn = document.getElementById("search-button");
-historyContainer = document.getElementById("history");
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,64 +3,74 @@ var historyContainer = document.getElementById('history');
 var currentCityDate = document.getElementById('current-city-date');
 var forecast = document.getElementById('forecast');
 var currentWeather = document.getElementById('current-weather');
+var cityInput = document.getElementById('city-input');
 
 
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + 'Chicago' + '&limit=1&appid=cc23144d939d4c97fd8e518be3041b20')
-    .then(function (responseGeo) {
-        return responseGeo.json();
-    })
-    .then(function (dataGeo) {
-        console.log(dataGeo);
-        for (var i = 0; i < dataGeo.length; i++) {
-            var lon = (dataGeo[i].lon);
-            var lat = (dataGeo[i].lat);
-            var nameEl = (dataGeo[i].name);
-            fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=cc23144d939d4c97fd8e518be3041b20&units=imperial')
-                .then(function (responseWeather) {
-                    return responseWeather.json();
-                })
-                .then(function (dataWeather) {
-                    currentCityDate.textContent = nameEl + " " + dataWeather.list[0].dt_txt;
-                    var icon = document.createElement('i');
-                    var temparature = document.createElement('p');
-                    var wind = document.createElement('p');
-                    var humidity = document.createElement('p');
-                    currentWeather.appendChild(icon)
-                    currentWeather.appendChild(temparature)
-                    currentWeather.appendChild(wind)
-                    currentWeather.appendChild(humidity)
-                    icon.textContent = dataWeather.list[0].weather.icon;
-                    temparature.textContent = "Temp: " + dataWeather.list[0].main.temp +"\u00B0 F";
-                    wind.textContent = "Wind: " + dataWeather.list[0].wind.speed + "mph";
-                    humidity.textContent = "Humidity: " + dataWeather.list[0].main.humidity +"%";
-                    for (var i = 0; i < dataWeather.list.length; i++) {
-                        //Only append elements for these items in the array to get 5 day forecast
-                        if (i===7 || i===15|| i===23|| i===31|| i===39) {
-                        var forecastContainer = document.createElement('div');
-                        var forecastCard = document.createElement('div');
-                        var dateForecast = document.createElement('p');
-                        var iconForecast = document.createElement('i');
-                        var temparatureForecast = document.createElement('p');
-                        var windForecast = document.createElement('p');
-                        var humidityForecast = document.createElement('p');
-                        forecastContainer.setAttribute('class', "column")
-                        forecastCard.setAttribute('class', "callout primary")
-                        forecast.appendChild(forecastContainer);
-                        forecastContainer.appendChild(forecastCard);
-                        forecastCard.appendChild(dateForecast);
-                        forecastCard.appendChild(iconForecast);
-                        forecastCard.appendChild(temparatureForecast);
-                        forecastCard.appendChild(windForecast);
-                        forecastCard.appendChild(humidityForecast);
-                        dateForecast.textContent = dataWeather.list[i].dt_txt;
-                        iconForecast.textContent = dataWeather.list[i].weather.icon;
-                        temparatureForecast.textContent = "Temp: " + dataWeather.list[i].main.temp +"\u00B0 F";
-                        windForecast.textContent = "Wind: " + dataWeather.list[i].wind.speed;
-                        humidityForecast.textContent = "Humidity: " + dataWeather.list[i].main.humidity +"%";
-                    }
-                    }
-                })
-
-        }
-    })
+searchBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (cityInput.value != "") {
+        var historyBtn = document.createElement('button');
+        historyBtn.setAttribute('value', cityInput.value)
+        historyBtn.setAttribute('class', "button secondary expanded");
+        historyContainer.appendChild(historyBtn);
+        historyBtn.textContent = cityInput.value;
+    }
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&limit=1&appid=cc23144d939d4c97fd8e518be3041b20')
+        .then(function (responseGeo) {
+            return responseGeo.json();
+        })
+        .then(function (dataGeo) {
+            console.log(dataGeo);
+            for (var i = 0; i < dataGeo.length; i++) {
+                var lon = (dataGeo[i].lon);
+                var lat = (dataGeo[i].lat);
+                var nameEl = (dataGeo[i].name);
+                fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=cc23144d939d4c97fd8e518be3041b20&units=imperial')
+                    .then(function (responseWeather) {
+                        return responseWeather.json();
+                    })
+                    .then(function (dataWeather) {
+                        currentCityDate.textContent = nameEl + " " + dataWeather.list[0].dt_txt;
+                        var icon = document.createElement('i');
+                        var temparature = document.createElement('p');
+                        var wind = document.createElement('p');
+                        var humidity = document.createElement('p');
+                        currentWeather.appendChild(icon);
+                        currentWeather.appendChild(temparature);
+                        currentWeather.appendChild(wind);
+                        currentWeather.appendChild(humidity);
+                        icon.textContent = dataWeather.list[0].weather.icon;
+                        temparature.textContent = "Temp: " + dataWeather.list[0].main.temp + "\u00B0 F";
+                        wind.textContent = "Wind: " + dataWeather.list[0].wind.speed + "mph";
+                        humidity.textContent = "Humidity: " + dataWeather.list[0].main.humidity + "%";
+                        for (var i = 0; i < dataWeather.list.length; i++) {
+                            //Only append elements for these items in the array to get 5 day forecast
+                            if (i === 7 || i === 15 || i === 23 || i === 31 || i === 39) {
+                                var forecastContainer = document.createElement('div');
+                                var forecastCard = document.createElement('div');
+                                var dateForecast = document.createElement('p');
+                                var iconForecast = document.createElement('i');
+                                var temparatureForecast = document.createElement('p');
+                                var windForecast = document.createElement('p');
+                                var humidityForecast = document.createElement('p');
+                                forecastContainer.setAttribute('class', "column")
+                                forecastCard.setAttribute('class', "callout primary")
+                                forecast.appendChild(forecastContainer);
+                                forecastContainer.appendChild(forecastCard);
+                                forecastCard.appendChild(dateForecast);
+                                forecastCard.appendChild(iconForecast);
+                                forecastCard.appendChild(temparatureForecast);
+                                forecastCard.appendChild(windForecast);
+                                forecastCard.appendChild(humidityForecast);
+                                dateForecast.textContent = dataWeather.list[i].dt_txt;
+                                iconForecast.textContent = dataWeather.list[i].weather.icon;
+                                temparatureForecast.textContent = "Temp: " + dataWeather.list[i].main.temp + "\u00B0 F";
+                                windForecast.textContent = "Wind: " + dataWeather.list[i].wind.speed;
+                                humidityForecast.textContent = "Humidity: " + dataWeather.list[i].main.humidity + "%";
+                            }
+                        }
+                    })
+            }
+        })
+})
 
